bccf9f01981c14f3f28d7eade08d7f3fa9919c75
Monther-Abu-Abedalla <m.abuabedallah@ahdtech.com>
2022-09-02 23:54:25 +0300

    HF

--- lib/feature/view/home_place_holder.dart
+++ lib/feature/view/home_place_holder.dart
@@ -1,10 +1,7 @@
-import 'package:ecommerce/feature/view/home_view/home_view.dart';
+
 import 'package:ecommerce/feature/view/widget/bottom_navigation.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/src/foundation/key.dart';
-import 'package:flutter/src/widgets/framework.dart';
 import 'package:get/get.dart';
-import 'package:get/get_state_manager/get_state_manager.dart';
 
 import '../view_model/home_view_model.dart';
 
