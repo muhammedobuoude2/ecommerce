e580d5c49c885b040f2ef6694a09abbb5a660590
Monther-Abu-Abedalla <m.abuabedallah@ahdtech.com>
2022-09-03 00:23:24 +0300

    hf

--- lib/feature/view_model/home_view_model.dart
+++ lib/feature/view_model/home_view_model.dart
@@ -1,6 +1,8 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:ecommerce/feature/view/cart_view/cart_view.dart';
 import 'package:ecommerce/feature/view/home_view/home_view.dart';
 import 'package:ecommerce/feature/view/profile_view/profile_view.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:get/get_state_manager/src/simple/get_controllers.dart';
 
 class HomeviewModel extends GetxController {
@@ -17,5 +19,14 @@ class HomeviewModel extends GetxController {
     ProfileView(),
   ];
 
+
+  //get  stream product by category id  from firebase
+  // Stream<List<Product>> getProductsByCategoryId(String categoryId) {
+  //   return FirebaseFirestore.instance
+  //       .collection('products')
+  //       .where('categoryId', isEqualTo: categoryId)
+  //       .snapshots()
+  //       .map((snapshot) => snapshot.docs.map((doc) => Product.fromMap(doc.data())).toList());
+  // }
   
 }
